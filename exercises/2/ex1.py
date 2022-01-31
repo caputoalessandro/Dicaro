b695d24dd0eaafbc4aebe0290a4695debb598cc2
acptIA <acpt1994@gmail.com>
2022-01-31 18:16:27 +0100

    segmentazione corretta

--- exercises/2/ex1.py
+++ exercises/2/ex1.py
@@ -48,45 +48,68 @@ def find(interval, sentences):
     for s1, s2 in interval:
         new_score = len(set(s1) & set(s2))
         if new_score == 0:
-            return sentences.index(s1)
-    return 1000
+            return sentences.index(s2)
+    return None
 
 
-def find_breakpoint(sentences, start):
-    forward_interval = zip(sentences[start:], sentences[start + 1:])
-    backword_interval = zip(sentences[:start], sentences[1:start])
+def get_stops(indexes, start):
+    forward_stop = backward_stop = start
+    index = indexes.index(start)
+
+    if index != (len(indexes)-1):
+        forward_stop = indexes[index + 1]
+
+    if index != 0:
+        backward_stop = indexes[index - 1] + 1
+
+    if index == 0:
+        backward_stop = 0
+
+    return forward_stop, backward_stop
+
+
+def find_breakpoint(sentences, start, indexes):
+    forward_stop, backward_stop = get_stops(indexes, start)
+    forward_interval = zip(sentences[start: forward_stop], sentences[start + 1:forward_stop])
+    backward_interval = zip(sentences[backward_stop:start], sentences[backward_stop:start-1])
     forward_bp = find(forward_interval, sentences)
-    backword_bp = find(backword_interval, sentences)
-    if abs(start - forward_bp) > abs(start - backword_bp):
-        return backword_bp
-    else:
+    backward_bp = find(backward_interval, sentences)
+
+    if backward_bp and forward_bp and abs(start - forward_bp) > abs(start - backward_bp):
+        return backward_bp
+    elif forward_bp:
         return forward_bp
+    else:
+        return start
 
 
 def get_bp_indexes(segments):
     bp_indexes = [(None, 0)]
     for segment in segments:
-        print()
         index = len(segment) - 1 + bp_indexes[-1][1]
         bp_indexes.append((segment, index))
     bp_indexes.remove((None, 0))
+    ind = [i[1] for i in bp_indexes]
     return bp_indexes
 
 
 def update_breakpoints(segments, sentences):
     segments_indexes = get_bp_indexes(segments)
-    res = [find_breakpoint(sentences, index) for segment, index in segments_indexes]
-    res.insert(0, 0)
-    res.append(len(sentences))
-    return res
+    indexes = [s[1] for s in segments_indexes]
+    res = [find_breakpoint(sentences, index, indexes) for segment, index in segments_indexes]
+
+    pairs=[]
+    pairs.append((0, res[0]))
+    for v1, v2 in zip(res, res[1:]):
+        pairs.append((v1+1, v2))
+    pairs.append((res[-1]+1, len(sentences)))
+
+    return pairs
 
 
 def update_segments(sentences, segments):
     new_bps = update_breakpoints(segments, sentences)
-    result = segments[new_bps[0]:new_bps[1]]
-    for start, stop in zip(new_bps[1:], new_bps[2:]):
-        result.append(sentences[start+1:stop])
-    return result
+    return [sentences[start:stop] for start, stop in new_bps]
 
 
 def get_segments(sentences, k):
@@ -97,16 +120,26 @@ def get_segments(sentences, k):
 def segmentation(text, iterations):
     sentences = get_sentences(text)
     segments = get_segments(sentences, 10)
-
     segmentation_results = []
 
     for i in range(iterations):
         segments = update_segments(sentences, segments)
+        segments = [segment for segment in segments if segment != []]
+        segs_indexes = get_bp_indexes(segments)
+        indexes = [s[1] for s in segs_indexes]
         new_score = get_segmentation_score(segments)
-        segmentation_results.append((segments, new_score))
+        segmentation_results.append((segments, new_score, indexes))
 
     best = max(segmentation_results, key=lambda x: x[1])
-    return best
+    return segmentation_results, best
+
+
+def print_results(segmentation_result, best):
+    for segment, score, indexes in segmentation_result:
+        print(indexes, score)
+    print("\nBest segments: ", best[1], best[2])
+    print("______________________________________________________________")
+    print("\n")
 
 
 def main():
@@ -114,8 +147,8 @@ def main():
     for article in articles_path.iterdir():
         with open(article) as f:
             text = f.read()
-            segments, score = segmentation(text, 10)
-            print(segments, score)
+            segmentation_result, best = segmentation(text, 10)
+            print_results(segmentation_result, best)
 
 
 if __name__ == "__main__":
